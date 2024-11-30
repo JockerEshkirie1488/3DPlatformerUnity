bfffe79ab3f318c4c21bae88a96a698e15965b04
JockerEshkirie1488 <166137098+JockerEshkirie1488@users.noreply.github.com>
2024-11-30 18:03:11 +0700

    Create SceneChanger.cs

---
+++ SceneChanger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneName;
+
+    void OnTriggerEnter()
+    {
+        EditorSceneManager.LoadScene(sceneName);
+    }
+}
